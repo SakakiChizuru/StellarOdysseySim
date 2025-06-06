9d81fcfb8fd297af1503079a6876195b75209580
SakakiChizuru <montrey@vip.qq.com>
2025-06-06 22:48:58 +0800

    Add "Available State Points" support for Optimizer

--- optimizer.js
+++ optimizer.js
@@ -39,29 +39,36 @@ class Optimizer {
     }
 
     optimize() {
-
         let hits_to_die;
         let hits_to_kill;
 
         if (this.player.n_clones < 5) {
             hits_to_die = 5;
             hits_to_kill = 7;
-        }
-        else {
+        } else {
             hits_to_die = 4;
             hits_to_kill = 8;
         }
 
         const total_hp_to_have = (hits_to_die - 1) * this.mob.dmg + 1;
-        const remaining_hp = Math.max(0.0, total_hp_to_have - this.player.shield_def);
+        const remaining_hp = Math.max(
+            0.0,
+            total_hp_to_have - this.player.shield_def
+        );
         const needed_hull = Math.ceil(remaining_hp / 7.0);
 
-        let weapon1 = this.player.weapon_ele1, weapon2 = this.player.weapon_ele2;
-        if ((weapon1 && (!weapon2 || weapon2 === 'None')) && weapon1 !== 'None') weapon2 = weapon1;
-        if ((weapon2 && (!weapon1 || weapon1 === 'None')) && weapon2 !== 'None') weapon1 = weapon2;
-        let shield1 = this.player.shield_ele1, shield2 = this.player.shield_ele2;
-        if ((shield1 && (!shield2 || shield2 === 'None')) && shield1 !== 'None') shield2 = shield1;
-        if ((shield2 && (!shield1 || shield1 === 'None')) && shield2 !== 'None') shield1 = shield2;
+        let weapon1 = this.player.weapon_ele1,
+            weapon2 = this.player.weapon_ele2;
+        if (weapon1 && (!weapon2 || weapon2 === "None") && weapon1 !== "None")
+            weapon2 = weapon1;
+        if (weapon2 && (!weapon1 || weapon1 === "None") && weapon2 !== "None")
+            weapon1 = weapon2;
+        let shield1 = this.player.shield_ele1,
+            shield2 = this.player.shield_ele2;
+        if (shield1 && (!shield2 || shield2 === "None") && shield1 !== "None")
+            shield2 = shield1;
+        if (shield2 && (!shield1 || shield1 === "None") && shield2 !== "None")
+            shield1 = shield2;
         const modifications = [weapon1, weapon2, shield1, shield2];
         const target_weaknesses = this.mob.weaknesses || [];
         let total_damage_modifier = 0.0;
@@ -72,10 +79,28 @@ class Optimizer {
         }
 
         const total_attack_to_have = this.mob.hp / hits_to_kill + 1;
-        const needed_pow = Math.ceil(Math.max(0.0, (total_attack_to_have - this.player.n_clones * this.player.weapon_dmg * (1 + total_damage_modifier)) / (7.0 * this.player.n_clones * (1 + total_damage_modifier))));
+        const needed_pow = Math.ceil(
+            Math.max(
+                0.0,
+                (total_attack_to_have -
+                    this.player.n_clones *
+                        this.player.weapon_dmg *
+                        (1 + total_damage_modifier)) /
+                    (7.0 * this.player.n_clones * (1 + total_damage_modifier))
+            )
+        );
 
-        const nb_points = Math.floor(this.player.pow + this.player.pre + this.player.eva + this.player.hull);
+        // 2025-06-06 Added available points
+        const nb_points = Math.floor(
+            this.player.pow +
+                this.player.pre +
+                this.player.eva +
+                this.player.hull +
+                this.player.availablePoints
+        );
+
         const available_points = nb_points - needed_pow - needed_hull;
+        
         const lcl_result = [];
 
         for (let p = 0; p <= available_points; p++) {
@@ -95,33 +120,56 @@ class Optimizer {
                 weapon_ele1: this.player.weapon_ele1,
                 weapon_ele2: this.player.weapon_ele2,
                 shield_ele1: this.player.shield_ele1,
-                shield_ele2: this.player.shield_ele2
+                shield_ele2: this.player.shield_ele2,
+                // 2025-06-06 Added available points
+                available: this.player.availablePoints,
             });
-            const battle = new Battle({ player: tmp_player, mob: this.mob, list_modifiers: this.list_modifiers });
+            const battle = new Battle({
+                player: tmp_player,
+                mob: this.mob,
+                list_modifiers: this.list_modifiers,
+            });
             const win_chance = battle.repeat_fights(this.n_fights);
-            lcl_result.push({ stats: [power, precision, evasion, hull], win_chance });
+            lcl_result.push({
+                stats: [power, precision, evasion, hull],
+                win_chance,
+            });
         }
-        const opt_results = lcl_result.sort((a, b) => b.win_chance - a.win_chance);
+        const opt_results = lcl_result.sort(
+            (a, b) => b.win_chance - a.win_chance
+        );
 
         if (opt_results.length === 0) {
             return null;
         }
-        return { bestStats: opt_results[0].stats, winChance: opt_results[0].win_chance };
+        return {
+            bestStats: opt_results[0].stats,
+            winChance: opt_results[0].win_chance,
+        };
     }
 
     findBestBuild(htd, htk, target = 'credits', verbose = false) {
         const total_hp_to_have = (htd - 1) * this.mob.dmg + 1;
-        const remaining_hp = Math.max(0.0, total_hp_to_have - this.player.shield_def);
+        const remaining_hp = Math.max(
+            0.0,
+            total_hp_to_have - this.player.shield_def
+        );
         const needed_hull = Math.ceil(remaining_hp / 7.0);
 
         let total_damage_modifier = 0.0;
 
-        let weapon1 = this.player.weapon_ele1, weapon2 = this.player.weapon_ele2;
-        if ((weapon1 && (!weapon2 || weapon2 === 'None')) && weapon1 !== 'None') weapon2 = weapon1;
-        if ((weapon2 && (!weapon1 || weapon1 === 'None')) && weapon2 !== 'None') weapon1 = weapon2;
-        let shield1 = this.player.shield_ele1, shield2 = this.player.shield_ele2;
-        if ((shield1 && (!shield2 || shield2 === 'None')) && shield1 !== 'None') shield2 = shield1;
-        if ((shield2 && (!shield1 || shield1 === 'None')) && shield2 !== 'None') shield1 = shield2;
+        let weapon1 = this.player.weapon_ele1,
+            weapon2 = this.player.weapon_ele2;
+        if (weapon1 && (!weapon2 || weapon2 === "None") && weapon1 !== "None")
+            weapon2 = weapon1;
+        if (weapon2 && (!weapon1 || weapon1 === "None") && weapon2 !== "None")
+            weapon1 = weapon2;
+        let shield1 = this.player.shield_ele1,
+            shield2 = this.player.shield_ele2;
+        if (shield1 && (!shield2 || shield2 === "None") && shield1 !== "None")
+            shield2 = shield1;
+        if (shield2 && (!shield1 || shield1 === "None") && shield2 !== "None")
+            shield1 = shield2;
         const modifications = [weapon1, weapon2, shield1, shield2];
         const target_weaknesses = this.mob.weaknesses || [];
         for (const mod of modifications) {
@@ -131,11 +179,26 @@ class Optimizer {
         }
 
         const total_attack_to_have = this.mob.hp / htk + 1;
-        const player_dmg = this.player.n_clones * this.player.weapon_dmg * (1 + total_damage_modifier);
+        const player_dmg =
+            this.player.n_clones *
+            this.player.weapon_dmg *
+            (1 + total_damage_modifier);
         const needed_power = Math.ceil(
-            Math.max(0.0, (total_attack_to_have - player_dmg) / (7.0 * this.player.n_clones * (1 + total_damage_modifier))));
+            Math.max(
+                0.0,
+                (total_attack_to_have - player_dmg) /
+                    (7.0 * this.player.n_clones * (1 + total_damage_modifier))
+            )
+        );
 
-        const nb_points = Math.floor(this.player.pow + this.player.pre + this.player.eva + this.player.hull);
+        // 2025-06-06 Added available points
+        const nb_points = Math.floor(
+            this.player.pow +
+                this.player.pre +
+                this.player.eva +
+                this.player.hull +
+                this.player.availablePoints
+        );
         const available_points = nb_points - needed_power - needed_hull;
 
         let best_build = [0, 0, 0, 0];
@@ -164,23 +227,37 @@ class Optimizer {
                 weapon_ele1: this.player.weapon_ele1,
                 weapon_ele2: this.player.weapon_ele2,
                 shield_ele1: this.player.shield_ele1,
-                shield_ele2: this.player.shield_ele2
+                shield_ele2: this.player.shield_ele2,
+                // 2025-06-06 Added available points
+                available: this.player.availablePoints,
             });
 
-            const battle = new Battle({ player: tmp_player, mob: this.mob, list_modifiers: this.list_modifiers });
+            const battle = new Battle({
+                player: tmp_player,
+                mob: this.mob,
+                list_modifiers: this.list_modifiers,
+            });
             const win_chance = battle.repeat_fights(this.n_fights);
 
             let res;
-            if (target === 'credits') {
-                res = battle.get_revenue('hourly', win_chance);
-            } else if (target === 'exp') {
-                res = battle.get_experience('hourly', win_chance, this.player.reputation || 0);
+            if (target === "credits") {
+                res = battle.get_revenue("hourly", win_chance);
+            } else if (target === "exp") {
+                res = battle.get_experience(
+                    "hourly",
+                    win_chance,
+                    this.player.reputation || 0
+                );
             } else {
                 throw new Error(`Invalid target ${target}`);
             }
 
             if (verbose) {
-                console.log(`[${power},${precision},${evasion},${hull}]: ${millify(res)} with ${(100 * win_chance).toFixed(3)}% win chance`);
+                console.log(
+                    `[${power},${precision},${evasion},${hull}]: ${millify(
+                        res
+                    )} with ${(100 * win_chance).toFixed(3)}% win chance`
+                );
             }
 
             if (res > best_res) {
